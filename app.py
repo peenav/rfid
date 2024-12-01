29f584e8fe56b1ee6236d95875ae84f682b5c1a0
peenav <141173786+peenav@users.noreply.github.com>
2024-12-01 15:29:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -203,4 +203,4 @@ def get_all_transactions():
     return jsonify(transactions)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=False)
